17212efd443b5da615dd9fde6140bffe5eac2fcc
anuluj <matego99@gmail.com>
2017-02-21 21:38:19 +0100

    initial commit

---
+++ KolkoIKrz/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace KolkoIKrz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_0_0 = new System.Windows.Forms.Button();
+            this.bt_0_1 = new System.Windows.Forms.Button();
+            this.bt_0_2 = new System.Windows.Forms.Button();
+            this.bt_1_0 = new System.Windows.Forms.Button();
+            this.bt_1_1 = new System.Windows.Forms.Button();
+            this.bt_1_2 = new System.Windows.Forms.Button();
+            this.bt_2_0 = new System.Windows.Forms.Button();
+            this.bt_2_1 = new System.Windows.Forms.Button();
+            this.bt_2_2 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_0_0
+            // 
+            this.bt_0_0.Location = new System.Drawing.Point(7, 6);
+            this.bt_0_0.Name = "bt_0_0";
+            this.bt_0_0.Size = new System.Drawing.Size(50, 50);
+            this.bt_0_0.TabIndex = 0;
+            this.bt_0_0.TabStop = false;
+            this.bt_0_0.UseVisualStyleBackColor = true;
+            this.bt_0_0.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bt_0_1
+            // 
+            this.bt_0_1.Location = new System.Drawing.Point(63, 6);
+            this.bt_0_1.Name = "bt_0_1";
+            this.bt_0_1.Size = new System.Drawing.Size(50, 50);
+            this.bt_0_1.TabIndex = 1;
+            this.bt_0_1.TabStop = false;
+            this.bt_0_1.UseVisualStyleBackColor = true;
+            this.bt_0_1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bt_0_2
+            // 
+            this.bt_0_2.Location = new System.Drawing.Point(119, 6);
+            this.bt_0_2.Name = "bt_0_2";
+            this.bt_0_2.Size = new System.Drawing.Size(50, 50);
+            this.bt_0_2.TabIndex = 2;
+            this.bt_0_2.TabStop = false;
+            this.bt_0_2.UseVisualStyleBackColor = true;
+            this.bt_0_2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bt_1_0
+            // 
+            this.bt_1_0.Location = new System.Drawing.Point(7, 62);
+            this.bt_1_0.Name = "bt_1_0";
+            this.bt_1_0.Size = new System.Drawing.Size(50, 50);
+            this.bt_1_0.TabIndex = 3;
+            this.bt_1_0.TabStop = false;
+            this.bt_1_0.UseVisualStyleBackColor = true;
+            this.bt_1_0.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bt_1_1
+            // 
+            this.bt_1_1.Location = new System.Drawing.Point(63, 62);
+            this.bt_1_1.Name = "bt_1_1";
+            this.bt_1_1.Size = new System.Drawing.Size(50, 50);
+            this.bt_1_1.TabIndex = 4;
+            this.bt_1_1.TabStop = false;
+            this.bt_1_1.UseVisualStyleBackColor = true;
+            this.bt_1_1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bt_1_2
+            // 
+            this.bt_1_2.Location = new System.Drawing.Point(119, 62);
+            this.bt_1_2.Name = "bt_1_2";
+            this.bt_1_2.Size = new System.Drawing.Size(50, 50);
+            this.bt_1_2.TabIndex = 5;
+            this.bt_1_2.TabStop = false;
+            this.bt_1_2.UseVisualStyleBackColor = true;
+            this.bt_1_2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bt_2_0
+            // 
+            this.bt_2_0.Location = new System.Drawing.Point(7, 118);
+            this.bt_2_0.Name = "bt_2_0";
+            this.bt_2_0.Size = new System.Drawing.Size(50, 50);
+            this.bt_2_0.TabIndex = 6;
+            this.bt_2_0.TabStop = false;
+            this.bt_2_0.UseVisualStyleBackColor = true;
+            this.bt_2_0.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bt_2_1
+            // 
+            this.bt_2_1.Location = new System.Drawing.Point(63, 118);
+            this.bt_2_1.Name = "bt_2_1";
+            this.bt_2_1.Size = new System.Drawing.Size(50, 50);
+            this.bt_2_1.TabIndex = 7;
+            this.bt_2_1.TabStop = false;
+            this.bt_2_1.UseVisualStyleBackColor = true;
+            this.bt_2_1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bt_2_2
+            // 
+            this.bt_2_2.Location = new System.Drawing.Point(119, 118);
+            this.bt_2_2.Name = "bt_2_2";
+            this.bt_2_2.Size = new System.Drawing.Size(50, 50);
+            this.bt_2_2.TabIndex = 8;
+            this.bt_2_2.TabStop = false;
+            this.bt_2_2.UseVisualStyleBackColor = true;
+            this.bt_2_2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(244, 6);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(128, 50);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Wyczysc";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 178);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.bt_2_2);
+            this.Controls.Add(this.bt_2_1);
+            this.Controls.Add(this.bt_2_0);
+            this.Controls.Add(this.bt_1_2);
+            this.Controls.Add(this.bt_1_1);
+            this.Controls.Add(this.bt_1_0);
+            this.Controls.Add(this.bt_0_2);
+            this.Controls.Add(this.bt_0_1);
+            this.Controls.Add(this.bt_0_0);
+            this.Name = "Form1";
+            this.Text = "Kolko i krzyzyk";
+            this.Click += new System.EventHandler(this.button_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_0_0;
+        private System.Windows.Forms.Button bt_0_1;
+        private System.Windows.Forms.Button bt_0_2;
+        private System.Windows.Forms.Button bt_1_0;
+        private System.Windows.Forms.Button bt_1_1;
+        private System.Windows.Forms.Button bt_1_2;
+        private System.Windows.Forms.Button bt_2_0;
+        private System.Windows.Forms.Button bt_2_1;
+        private System.Windows.Forms.Button bt_2_2;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ KolkoIKrz/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KolkoIKrz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            wyczyscPlansze();
+
+        }
+        public int Kolejka = 0;
+        int[,] plansza = new int[3, 3];
+        int sumaPrzekotna1;
+        int sumaPrzekotna2;
+        private void button_Click(object sender, EventArgs e)
+        {
+
+            if (sender is Button)
+            {
+                Button button = (Button)sender;
+                string[] rozbitaNazwa = button.Name.Split('_');
+                int wiersz = int.Parse(rozbitaNazwa[1]);
+                int kolumna = int.Parse(rozbitaNazwa[2]);
+
+                if (button.Text == "" && Kolejka % 2 == 0)
+                {
+                    button.Text = "o";
+                    Kolejka++;
+                    button.Enabled = false;
+                    button.BackColor = Color.FromArgb(255, 0, 0);
+                    plansza[wiersz, kolumna] = 3;
+
+                }
+                else if (button.Text == "" && Kolejka % 2 != 0)
+                {
+                    button.Text = "x";
+                    Kolejka++;
+                    button.Enabled = false;
+                    button.BackColor = Color.LightSkyBlue;
+                    plansza[wiersz, kolumna] = 10;
+                }
+                else
+                {
+                    MessageBox.Show("Bład");
+                }
+            }
+            sumaPrzekotna1 = plansza[0, 0] + plansza[1, 1] + plansza[2, 2];
+            sumaPrzekotna2 = plansza[2, 0] + plansza[1, 1] + plansza[0, 2];
+            for (int i = 0; i < 3; i++)
+            {
+                int sumaWiersze = 0;
+                int sumaKolumny = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    sumaWiersze += plansza[i, j];
+                    sumaKolumny += plansza[i, j];
+                }
+                if (sumaWiersze == 9 || sumaKolumny == 9 || sumaPrzekotna1 == 9 || sumaPrzekotna2 == 9)
+                {
+                    MessageBox.Show("wygral o");
+                    wyczyscPlansze();
+                }
+                else if (sumaWiersze == 30 || sumaKolumny == 30 || sumaPrzekotna1 == 30 || sumaPrzekotna2 ==30)
+                {                   
+                    MessageBox.Show("wygral x");
+                    wyczyscPlansze();
+                }
+                else if(Kolejka == 9)
+                {
+                    MessageBox.Show("Nikt nie wygrał");
+                    wyczyscPlansze();
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            wyczyscPlansze();
+        }
+
+        private void wyczyscPlansze()
+        {
+            foreach (Control kontrolka in this.Controls)
+            {
+                if (kontrolka is Button)
+                {
+                    if (kontrolka.Text != "Wyczysc")
+                    {
+                        kontrolka.Enabled = true;
+                        kontrolka.Text = "";
+                        kontrolka.BackColor = Color.White;
+                    }
+                }
+            }
+            Array.Clear(plansza, 0, plansza.Length);
+            sumaPrzekotna1 = 0;
+            sumaPrzekotna2 = 0;
+            Kolejka = 0;
+        }
+    }
+}
